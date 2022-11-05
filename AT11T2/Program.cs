09cf592c39b6802ddd036211738654944b10bb17
lfons <lfons@DESKTOP-8PJVQIC>
2022-11-04 22:36:37 -0300

    Adicionar arquivos de projeto.

---
+++ AT11T2/Program.cs
@@ -0,0 +1,25 @@
+﻿int c;
+double num, soma, max;
+Console.Write("Quantos numeros pretende digitar: ");
+max = Convert.ToDouble(Console.ReadLine());
+soma = 0;
+Console.WriteLine("Digite " + max + " números: ");
+for (c = 1; c<= max; c++)
+{
+    Console.Write("digite o " + c + "º numero: ");
+    num = Convert.ToDouble(Console.ReadLine());
+    if (num > 10)
+    {
+        soma = soma + num;
+    }
+    
+    
+}
+Console.WriteLine(soma);
+
+
+
+Console.ReadKey();
+
+
+   
